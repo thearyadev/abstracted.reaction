f73cf266b5562fad1c1f31791d9b2b5c77c1f107
Aryan Kothari <personal@aryankothari.dev>
2023-11-26 00:57:57 +0000

    add impls for endpoints

--- backend/server/__main__.py
+++ backend/server/__main__.py
@@ -10,11 +10,14 @@ import time
 from pathlib import Path
 import shutil
 from uuid import UUID, uuid4
-from fastapi import APIRouter, FastAPI, File, Form, Query, UploadFile
+from fastapi import APIRouter, FastAPI, File, Form, Query, UploadFile, HTTPException
 from fastapi.responses import FileResponse, HTMLResponse, Response, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from util.models.film import FilmNoBytes
 from typing import Any, MutableMapping
+from util.models.uuid import RecordUUIDLike
+from util.models.rating import Rating
+from util.models.actress_detail import ActressDetail
 
 
 class StaticFileHandler(StaticFiles):
@@ -45,19 +48,66 @@ class Server:
         self.host = host
         self.port = port
         self.db = database_autoconfigure()
+        self.cache = DatabaseReadCache()
         self.configure_routes()
-        self.app.include_router(self.router)
 
-        self.app.mount(
-            "/", StaticFileHandler(directory="./server/build", html=True), name="static"
-        )
+        self.app.include_router(self.router)
+        # self.app.mount(
+        #     "/", StaticFileHandler(directory="./server/build", html=True), name="static"
+        # )
 
     def configure_routes(self) -> None:
-        ...
+        self.router.add_api_route(
+            "/films",
+            self.get_all_films,
+            methods=["GET"],
+        )
+        self.router.add_api_route("/film", self.get_single_film, methods=["get"])
 
     def run(self) -> Server:
         uvicorn.run(self.app, host=self.host, port=self.port)
         return self
+
+    def get_all_films(self) -> list[FilmNoBytes]:
+        if (latestStamp := self.db.get_latest_commit_uuid()) != self.cache.filmsStamp:
+            self.cache.filmsStamp, self.cache.films = (
+                latestStamp,
+                self.db.get_all_films(),
+            )
+        return self.cache.films
+
+    def get_single_film(self, uuid: RecordUUIDLike = Query(...)) -> FilmNoBytes:
+        if retrievedEntry := self.db.get_single_film(uuid):
+            return retrievedEntry
+        raise HTTPException(status_code=404, detail="film not found")
+
+    def get_image(self, uuid: RecordUUIDLike = Query(...)) -> bytes:
+        return NotImplemented
+
+    def set_rating(
+        self, uuid: RecordUUIDLike = Query(...), rating: Rating = Query(...)
+    ) -> Response:
+        return NotImplemented
+
+    def delete_film(self, uuid: RecordUUIDLike = Query(...)) -> Response:
+        return NotImplemented
+
+    def set_watch_status(
+        self, uuid: RecordUUIDLike = Query(...), new_watch_status: bool = Query(...)
+    ) -> Response:
+        return NotImplemented
+
+    def get_actress_list(self) -> list[str]:
+        return NotImplemented
+
+    def get_actress_detail(self, name: str = Query(...)) -> ActressDetail:
+        return NotImplemented
+
+    def serve_video(self, uuid: RecordUUIDLike = Query(...)) -> FileResponse:
+        return NotImplemented
+
+    def serve_root(self, *_) -> HTMLResponse:
+        return NotImplemented
 
 
 def main() -> int:
