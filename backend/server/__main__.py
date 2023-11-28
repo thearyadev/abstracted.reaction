f3e9a000369e820b0fa0b4d57359ab5ebd65b20a
Aryan Kothari <personal@aryankothari.dev>
2023-11-28 23:08:40 +0000

    implement missing endpoints

--- backend/server/__main__.py
+++ backend/server/__main__.py
@@ -2,14 +2,23 @@ from __future__ import annotations
 
 import uvicorn
 import fastapi
-from util.database.database import database_autoconfigure
+from util.database.database import Database
 import datetime
 import logging
 import os
 import time
 from pathlib import Path
 import shutil
-from fastapi import APIRouter, FastAPI, File, Form, Query, UploadFile, HTTPException
+from fastapi import (
+    APIRouter,
+    FastAPI,
+    File,
+    Form,
+    Query,
+    UploadFile,
+    HTTPException,
+    Body,
+)
 from fastapi.responses import FileResponse, HTMLResponse, Response, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from util.models.film import FilmNoBytes
@@ -18,6 +27,8 @@ from util.models.uuid import RecordUUIDLike
 from util.models.rating import Rating
 from util.models.actress_detail import ActressDetail
 from typing import Literal
+from fastapi.testclient import TestClient
+from typing import Annotated
 
 
 class StaticFileHandler(StaticFiles):
@@ -41,12 +52,12 @@ class DatabaseReadCache:
 
 
 class Server:
-    def __init__(self, host: str, port: int):
+    def __init__(self, host: str, port: int, db: Database | None = None):
         self.app = FastAPI()
         self.router = APIRouter(prefix="/api")
         self.host = host
         self.port = port
-        self.db = database_autoconfigure()
+        self.db = Database.from_env(load_dot_env=True)
         self.cache = DatabaseReadCache()
         self.configure_routes()
 
@@ -55,16 +66,48 @@ class Server:
         #     "/", StaticFileHandler(directory="./server/build", html=True), name="static"
         # )
 
+    def test_client(self) -> TestClient:
+        return TestClient(self.app)
+
     def configure_routes(self) -> None:
         self.router.add_api_route(
-            "/films",
+            "/get/films",
             self.get_all_films,
             methods=["GET"],
         )
-        self.router.add_api_route("/film", self.get_single_film, methods=["GET"])
-        self.router.add_api_route("/image", self.get_image, methods=["GET"])
-
-        self.router.add_api_route("/delete", self.delete_film, methods=["DELETE"])
+        self.router.add_api_route(
+            "/get/film",
+            self.get_single_film,
+            methods=["GET"],
+            responses={404: {"description": "film not found"}},
+        )
+        self.router.add_api_route(
+            "/get/image",
+            self.get_image,
+            methods=["GET"],
+            responses={404: {"description": "film not found"}},
+        )
+        self.router.add_api_route(
+            "/set/rating",
+            self.set_rating,
+            methods=["POST"],
+            responses={404: {"description": "rating not found"}},
+        )
+        self.router.add_api_route(
+            "/set/delete",
+            self.delete_film,
+            methods=["POST"],
+            responses={404: {"description": "film not found"}},
+        )
+        self.router.add_api_route(
+            "/set/watched",
+            self.set_watch_status,
+            methods=["POST"],
+            responses={404: {"description": "film not found"}},
+        )
+        self.router.add_api_route(
+            "/get/actresses", self.get_actress_list, methods=["GET"]
+        )
 
     def run(self) -> Server:
         logging.info(f"Starting uvicorn server on {self.host}:{self.port}")
@@ -106,21 +149,32 @@ class Server:
 
         return Response(image, media_type="image/png")
 
-    def set_rating(
-        self, uuid: RecordUUIDLike = Query(...), rating: Rating = Query(...)
-    ) -> Response:
-        return NotImplemented
+    def set_rating(self, rating: Annotated[Rating, Body(embed=True)]) -> Response:
+        try:
+            self.db.update_rating(rating)
+            return Response(status_code=200)
+        except ValueError:
+            raise HTTPException(status_code=404, detail="rating not found")
 
-    def delete_film(self, uuid: RecordUUIDLike = Query(...)) -> Response:
-        return NotImplemented
+    def delete_film(self, film: Annotated[FilmNoBytes, Body(embed=True)]) -> Response:
+        try:
+            self.db.delete_film(film)
+            return Response(200)
+        except ValueError:
+            raise HTTPException(status_code=404, detail="film not found")
 
     def set_watch_status(
-        self, uuid: RecordUUIDLike = Query(...), new_watch_status: bool = Query(...)
+        self, watch_status: bool = Query(...), uuid: RecordUUIDLike = Query(...)
     ) -> Response:
-        return NotImplemented
+        film = self.db.get_single_film(uuid)
+        if not film:
+            raise HTTPException(status_code=404, detail="film not found")
+        film.watched = watch_status
+        self.db.update_film(film)
+        return Response(status_code=200)
 
     def get_actress_list(self) -> list[str]:
-        return NotImplemented
+        return self.db.get_actress_list()
 
     def get_actress_detail(self, name: str = Query(...)) -> ActressDetail:
         return NotImplemented
@@ -132,7 +186,7 @@ class Server:
         return NotImplemented
 
 
-def main() -> int:
+def main() -> int:  # pragma: no cover
     server = Server(host="0.0.0.0", port=8112).run()
     return 0
 
